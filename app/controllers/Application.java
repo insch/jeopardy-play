5ab2ab7974bea19b5890b89dbb10eef1cab196d7
insch <inschi123@gmail.com>
2015-05-02 14:13:42 +0200

    Testing IntelliJ commit

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -8,7 +8,7 @@ import views.html.*;
 public class Application extends Controller {
 
     public static Result index() {
-        return ok(index.render("Markus du schwuchtel :D."));
+        return ok(index.render("Changes v0"));
     }
 
 }
